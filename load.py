05caa642f385ad5552ea8488e0a49609f6516186
Loïc Herman <mail@147lo.ch>
2020-02-06 12:38:58 +0100

    chore: bump version

--- load.py
+++ load.py
@@ -2,7 +2,7 @@
 The Sagittarius Eye EDMC plugin
 :author: CMDR 147loch
 :date: 05.02.2020
-:version: 1.1.6
+:version: 1.1.8
 """
 
 import sys
@@ -18,7 +18,7 @@ from config import config
 this = sys.modules[__name__]
 
 this.plugin = "edmc-sagi"
-this.version = "1.1.6"
+this.version = "1.1.8"
 this.client_version = "{}-{}".format(this.plugin, this.version)
 
 
